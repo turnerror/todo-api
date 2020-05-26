9a5cb5f96e13380b10f35d00e7b9e148f16fd4fd
turnerror <christianturner@hotmail.co.uk>
2020-05-26 14:01:35 +0100

    added routes for todo app

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Welcome to my todo app'));
 
-app.get('/user/:id', (req, res) => {
-    let name = req.query.name;
-    let id = req.params.id;
+app.get('/todo', (req, res) => {
+    let deleted = req.query.deleted;
 
-    res.send('Hello ' + id + '!')
+    res.send('Delete = 1 or 0')
 });
 
-app.post('/user', jsonParser, (req, res) => {
-    //some code here
+app.post('/todo', jsonParser, (req, res) => {
+    //code to create todo here
     let body = req.body;// how we access our body information after the parser.
-    res.send('created a user!');
+    res.send('created a todo!');
+});
+
+app.put('/todo/:id', jsonParser, (req, res) => {
+    //code to mark todo done here
+    let body = req.body;// how we access our body information after the parser.
+    res.send('created a todo!');
+});
+
+app.delete('/todo/:id', jsonParser, (req, res) => {
+    //some code to delete todo here
+    let body = req.body;// how we access our body information after the parser.
+    res.send('created a todo!');
 });
 
 
