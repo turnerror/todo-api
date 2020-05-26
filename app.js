559949a7f35fd04191be2fe35d73ae5aba144eeb
turnerror <christianturner@hotmail.co.uk>
2020-05-26 14:11:44 +0100

    updated routes

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+var jsonParser = bodyParser.json();
 
-app.get('/', (req, res) => res.send('Welcome to my todo app'));
-
-app.get('/todo', (req, res) => {
+app.get('/todos', (req, res) => {
     let deleted = req.query.deleted;
 
-    res.send('Delete = 1 or 0')
+    res.send('Get all todos')
 });
 
-app.post('/todo', jsonParser, (req, res) => {
+app.post('/todos', jsonParser, (req, res) => {
     //code to create todo here
     let body = req.body;
     res.send('created a todo!');
 });
 
-app.put('/todo', jsonParser, (req, res) => {
+app.put('/todos', jsonParser, (req, res) => {
     //some code to mark all selected todos as complete
     let body = req.body;
-    res.send('deleted select todos!');
+    res.send('updated selected todos!');
 });
 
-app.delete('/todo', jsonParser, (req, res) => {
+app.delete('/todos', jsonParser, (req, res) => {
     //some code to delete all selected todos here
     let body = req.body;
-    res.send('deleted select todos!');
+    res.send('deleted selected todos!');
 });
 
 
