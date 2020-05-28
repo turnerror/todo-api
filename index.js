97849fd36aea68d6136bc564e5337c64a18b3138
turnerror <christianturner@hotmail.co.uk>
2020-05-28 14:30:42 +0100

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./Config/routes');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // enable CORS
 app.use(function(req, res, next) {
