7becda3dc3bf6c5b1c96df48093741697a120e3f
turnerror <christianturner@hotmail.co.uk>
2020-05-28 21:25:06 +0100

    deleted utilities.js after reformatting routes

--- Services/TodosService/Utilities.js
+++
@@ -1,7 +0,0 @@
-const ObjectId = require('mongodb').ObjectId;
-
-function idsToObjectIds(ids){
-    return ids.map(id => ObjectId(id));
-}
-
-module.exports.idsToObjectIds = idsToObjectIds;
--- Services/TodosService/index.js
+++ Services/TodosService/index.js
@@ -1,3 +1,2 @@
 exports.validation = require('./Validation');
 exports.database = require('./Database');
-exports.utilities = require('./Utilities');
